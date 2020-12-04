5b8242e86e98c27bab047e3cbca259b35c0aeb21
Sparsh Gupta <44977909+sparshgupta2212@users.noreply.github.com>
2020-12-04 03:07:20 -0800

    Add files via upload

---
+++ start.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $(".continue-btn").click(function(){
+      $(".sec1").fadeOut();
+      $(".sec2").fadeIn();
+    });
+    $(".start-quiz").click(function(){
+      name = $(".input").val();
+      if(name === '') {
+       alert("Please Enter Your Name")
+       return;
+      }
+      
+      $(".sec2").fadeOut();
+      
+    });
+
+   
+});
